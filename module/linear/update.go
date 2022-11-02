37295cf0c7096e173f998370cf072c1d85f9c331
xh3b4sd <xh3b4sd@gmail.com>
2022-11-02 16:30:46 +0100

    fix

--- module/linear/update.go
+++ module/linear/update.go
@@ -7,10 +7,9 @@ import (
 
 func (m *Module) Update(vec getlin.Vector) {
 	for i := range m.cla {
-		x := []bool{vec.Out().Raw()[i]}
 		m.cla[i].Update(vector.New(vector.Config{
 			Inp: vec.Inp().Raw(),
-			Out: x,
+			Out: []bool{vec.Out().Raw()[i]},
 		}))
 	}
 }
