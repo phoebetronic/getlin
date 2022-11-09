348cff245e3157e2d88e83f56f33c7742ec1ebca
xh3b4sd <xh3b4sd@gmail.com>
2022-11-09 11:28:53 +0100

    fix

--- vector/config.go
+++ vector/config.go
@@ -4,11 +4,9 @@ type Config struct {
 	// Cla provides optional class identifier information for different Module
 	// implementations to leverage internally.
 	Cla int
-	// Inp carries the input bits consumed by any given Module, be it inference
-	// or training.
+	// Inp carries the configured feature weights of this Vector.
 	Inp []float32
-	// Out provides either the externally defined true labels or carries the
-	// predicted labels.
+	// Out carries the true weights or weight predictions of this Vector.
 	Out []float32
 }
 
